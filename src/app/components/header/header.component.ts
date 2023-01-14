20b4f7074a8a5a8140c7e281a39072156119a4e1
Neeraj <neerajmilan968@gmail.com>
2023-01-14 22:35:08 +0530

    commented audio

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,20 +9,20 @@ export class HeaderComponent implements OnInit{
   audioFlag = false;
   audio = new Audio()
   ngOnInit(): void {
-    this.audio.src = "../../assets/audio/on_my_way_64.mp3";
-    this.audio.muted;
-    this.audio.load()
+    // this.audio.src = "../../assets/audio/on_my_way_64.mp3";
+    // this.audio.muted;
+    // this.audio.load()
     
   }
-  
+
   toggleAudio(){  
     this.audioFlag = !this.audioFlag;  
-    if(this.audioFlag){
-      this.audio.play()
-    }
-    else{
-      this.audio.pause()
-    }
+    // if(this.audioFlag){
+    //   this.audio.play()
+    // }
+    // else{
+    //   this.audio.pause()
+    // }
     
   }
 
